01d6794bae97ca21ca144a4cbe1e954da5906741
iagolaguna <iagolaguna.83@gmail.com>
2019-01-05 17:52:38 -0200

    learning how to handle event

--- src/app.js
+++ src/app.js
@@ -2,16 +2,13 @@
 
 import React, { Component } from 'react'
 import Square from './square'
-
 class App extends Component {
-
-  render() {
+  render () {
     return (
-      <div className='container'>
-        {['blue', 'red', 'blue'].map((square,index) => (
-          <Square key={index} color={square} />
-        ))}
-
+      <div className='container' onClick={(e) => {
+        alert('clicou')
+      }}>
+        <Square />
       </div>
     )
   }
