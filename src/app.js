363e397d8f75e5c5f8edc1da2c0295f225b036d9
iagolaguna <iagolaguna.83@gmail.com>
2019-01-08 23:19:09 -0200

    learning about lifecyle of mount and unmount

--- src/app.js
+++ src/app.js
@@ -1,40 +1,31 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
-import Square from './square'
+import Timer from './timer'
 class App extends Component {
   constructor() {
+    console.log('constructor')
     super()
     this.state = {
-      color: 'green'
+      showTimer: true
     }
+  }
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
   }
 
   render () {
+    console.log('render')
     return (
       <div>
-        <Square color={this.state.color} />
-        {['red', 'green', 'blue'].map((color) => (
-          <Button
-            key={color}
-            handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-        ))}
-      </div>
+        {this.state.showTimer && <Timer />}
+        <button onClick={() => (this.setState({ showTimer: !this.state.showTimer }))}>Show/Hide timer</button>
+      </div >
     )
   }
 }
-
-// var App = React.createClass({
-//   render: function () {
-//     return (
-//       <div className='container'>
-//         <Title name='Fernado' lastname='Daciuk'/>
-//       </div>
-//     )
-//   }
-// })
-
 export default App
