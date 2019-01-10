0bb52f5786f2096605a32b30a3b8000b468b1397
iagolaguna <iagolaguna.83@gmail.com>
2019-01-09 22:32:48 -0200

    learning about select

--- src/app.js
+++ src/app.js
@@ -6,32 +6,22 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false
+      value: '2',
     }
   }
   render () {
-    console.log('render app')
     return (
       <div>
         <form>
-          <input type='text' value={this.state.value}
-            onChange={(e) => {
-              this.setState({ value: e.target.value })
-            }}
-          />
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.checked}
-              onChange={(e) => {
-                this.setState({ checked: e.target.checked })
-              }}
-            />
-            Checkbox
-          </label>
-          <input type='radio' name='rd' value='1' defaultChecked />Radio 1
-          <input type='radio' name='rd' value='2' />Radio 2
+          <select value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }}>
+            <option value='1'>Opção 1</option>
+            <option value='2'>Opção 2</option>
+            <option value='3'>Opção 3</option>
+          </select>
         </form>
       </div>
     )
