a602c3f7a38715b562ea502f928068cd87f818cd
iagolaguna <iagolaguna.83@gmail.com>
2019-01-09 22:15:08 -0200

    learning about checkbox and radio

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'Teste'
+      value: 'Valor inicial',
+      checked: false
     }
   }
   render () {
@@ -14,11 +15,23 @@ class App extends Component {
     return (
       <div>
         <form>
-          <input type='text' value={this.state.value} 
-          onChange={(e) => {
-            this.setState({ value: e.target.value })
-          }} 
+          <input type='text' value={this.state.value}
+            onChange={(e) => {
+              this.setState({ value: e.target.value })
+            }}
           />
+          <label>
+            <input
+              type='checkbox'
+              checked={this.state.checked}
+              onChange={(e) => {
+                this.setState({ checked: e.target.checked })
+              }}
+            />
+            Checkbox
+          </label>
+          <input type='radio' name='rd' value='1' defaultChecked />Radio 1
+          <input type='radio' name='rd' value='2' />Radio 2
         </form>
       </div>
     )
